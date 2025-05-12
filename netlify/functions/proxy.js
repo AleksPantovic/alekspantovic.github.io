36c0fc3917f3f5c053a924a1dd2e8a3cfd752c4e
AleksPantovic <aleksandar.pantovic@asioso.com>
2025-05-12 16:59:16 +0200

    small changes

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -6,10 +6,10 @@ exports.handler = async (event) => {
 
     // Forward the headers from the request
     const headers = {
-      'Authorization': event.headers['authorization'] || 'Bearer <ACCESS_TOKEN>', // Replace <ACCESS_TOKEN> with a valid token
-      'X-Client-ID': event.headers['x-client-id'] || '<X_COYO_CLIENT_ID>', // Replace with your client ID
-      'X-Coyo-Current-User': event.headers['x-coyo-current-user'] || '<X_COYO_CURRENT_USER>', // Replace with the current user ID
-      'X-Csrf-Token': event.headers['x-csrf-token'] || '<X_CSRF_TOKEN>', // Replace with a valid CSRF token
+      'Authorization': 'Bearer <ACCESS_TOKEN>', // Replace <ACCESS_TOKEN> with a valid token
+      'X-Client-ID': '<X_COYO_CLIENT_ID>', // Replace with your client ID
+      'X-Coyo-Current-User': '<X_COYO_CURRENT_USER>', // Replace with the current user ID
+      'X-Csrf-Token': '<X_CSRF_TOKEN>', // Replace with a valid CSRF token
       'Accept-Version': '1.5.0',
       'Accept': 'application/json',
     };
