9aeefd0974f3dd8c1585fd5d62733e27c104c5e1
AleksPantovic <aleksandar.pantovic@asioso.com>
2025-05-14 14:49:10 +0200

    netlify changes

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -2,27 +2,53 @@ const axios = require('axios');
 
 exports.handler = async (event) => {
   try {
-    // Prepare headers using secrets from Netlify env vars
+    // 1. Get JWT token from lifecycle event (assume POST with JSON body)
+    let jwtToken;
+    if (event.httpMethod === 'POST') {
+      const body = JSON.parse(event.body || '{}');
+      jwtToken = body.token;
+    } else {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: 'Lifecycle JWT token required in POST body' }),
+        headers: { 'Content-Type': 'application/json' }
+      };
+    }
+
+    // 2. Exchange JWT for OAuth access token
+    const clientId = 'organization';
+    const clientSecret = '81dd0c6a-6fd9-43ff-878c-21327b07ae1b';
+    const tokenUrl = 'https://asioso.coyocloud.com/api/oauth/token';
+
+    const tokenResponse = await axios.post(tokenUrl, new URLSearchParams({
+      grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+      assertion: jwtToken,
+      client_id: clientId,
+      client_secret: clientSecret,
+    }), {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    });
+
+    const accessToken = tokenResponse.data.access_token;
+
+    // 3. Use the access token to fetch /api/users
+    const haiiloApiUrl = 'https://asioso.coyocloud.com/api/users';
     const headers = {
-      'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`,
-      'X-Client-ID': process.env.X_COYO_CLIENT_ID,
-      'X-Coyo-Current-User': process.env.X_COYO_CURRENT_USER,
-      'X-Csrf-Token': process.env.X_CSRF_TOKEN,
+      'Authorization': `Bearer ${accessToken}`,
       'Accept-Version': '1.5.0',
       'Accept': 'application/json',
     };
 
-    // Make the request to Haiilo API
-    const response = await axios.get('https://asioso.coyocloud.com/api/users', { headers });
+    const response = await axios.get(haiiloApiUrl, { headers });
 
-    // Return the data to the frontend
     return {
       statusCode: response.status,
       body: JSON.stringify(response.data),
       headers: { 'Content-Type': 'application/json' }
     };
   } catch (error) {
-    // Return error info as JSON
     return {
       statusCode: error.response?.status || 500,
       body: JSON.stringify({
